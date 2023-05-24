1903d069c39f814f90948a973498fd1d253b3803
SANJAIRAGUL-MURUGESAN <121217742+SANJAIRAGUL-MURUGESAN@users.noreply.github.com>
2023-05-24 21:58:25 +0530

    Update mongoose.js

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const conn = mongoose.createConnection('mongodb://localhost:27017')
+const conn = mongoose.createConnection('mongodb+srv://user1:PAnKAkpI6VnCuPcR@cluster0.7jaef.mongodb.net/?retryWrites=true&w=majority')
 
 const db = conn.useDb("NEW")
 
